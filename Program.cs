684216ae4a885883ba5ef058e957872d395c4b96
Petrus Silva <petrusjuliano@bol.com.br>
2024-01-08 15:31:18 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new("8348488", "Tijolo", "14M7", 64);
+nokia.SobreSmartphone();
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone");
+Iphone iphone = new("456893", "15 Pro", "4566L6", 256);
+iphone.SobreSmartphone();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
